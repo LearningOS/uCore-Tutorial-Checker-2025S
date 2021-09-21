80a604e7abe6720994c93741dae2860e2ef8c1c0
DeathWish5 <zyr_ms@outlook.com>
2021-09-21 19:38:38 +0800

    minor update

--- check/ch3.py
+++ check/ch3.py
@@ -8,25 +8,29 @@ EXPECTED += [
     r"current time_msec = (\d+)",
     r"time_msec = (\d+) after sleeping (\d+) ticks, delta = (\d+)ms!",
     "Test sleep1 passed!",
-    "AAAAAAAAAA [1/5]",
-    "BBBBBBBBBB [1/5]",
-    "CCCCCCCCCC [1/5]",
-    "AAAAAAAAAA [2/5]",
-    "BBBBBBBBBB [2/5]",
-    "CCCCCCCCCC [2/5]",
-    "AAAAAAAAAA [3/5]",
-    "BBBBBBBBBB [3/5]",
-    "CCCCCCCCCC [3/5]",
-    "AAAAAAAAAA [4/5]",
-    "BBBBBBBBBB [4/5]",
-    "CCCCCCCCCC [4/5]",
-    "AAAAAAAAAA [5/5]",
-    "BBBBBBBBBB [5/5]",
-    "CCCCCCCCCC [5/5]",
+]
+
+EXPECTED += [
+    # "AAAAAAAAAA [1/5]",
+    # "BBBBBBBBBB [1/5]",
+    # "CCCCCCCCCC [1/5]",
+    # "AAAAAAAAAA [2/5]",
+    # "BBBBBBBBBB [2/5]",
+    # "CCCCCCCCCC [2/5]",
+    # "AAAAAAAAAA [3/5]",
+    # "BBBBBBBBBB [3/5]",
+    # "CCCCCCCCCC [3/5]",
+    # "AAAAAAAAAA [4/5]",
+    # "BBBBBBBBBB [4/5]",
+    # "CCCCCCCCCC [4/5]",
+    # "AAAAAAAAAA [5/5]",
+    # "BBBBBBBBBB [5/5]",
+    # "CCCCCCCCCC [5/5]",
     "Test write A OK!",
     "Test write B OK!",
     "Test write C OK!",
 ]
+
 
 if __name__ == '__main__':
     base.test(EXPECTED + TEMP, NOT_EXPECTED)
