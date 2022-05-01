0bf3ad0b42080b26439271ca488c7b4f5899d061
rcy17 <rcycyzh@163.com>
2022-05-01 11:52:38 +0800

    Update checker for ch6

--- check/ch7.py
+++ check/ch7.py
@@ -2,13 +2,9 @@ import base
 from ch6 import EXPECTED, NOT_EXPECTED
 
 EXPECTED += [
-    "Test file0 OK!",
-    "Test fstat OK!",
-    "Test link OK!",
 ]
 
 TEMP = [
-    "ch7 Usertests passed!",
 ]
 
 if __name__ == '__main__':
